9f27e4caada472aff8c459c23b9c10af4d1ca66d
Wizard-Fingerz <adewale.oladiti28@gmail.com>
2024-02-04 22:03:27 +0100

    Login pages completed

---
+++ src/pages/examiner-login.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import styles from "./page.module.css";
+import { API_BASE_URL } from '../constants';
+
+function LoginPage() {
+    const router = useRouter();
+
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        // Prepare the login data
+        const loginData = {
+            username: username,
+            password: password,
+        };
+
+        // Send a POST request to your Django API endpoint
+        try {
+            const response = await fetch(`${API_BASE_URL}/login/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                if (data.user_type === 'examiner') {
+                  // Display the token in the console
+                  console.log('Token:', data.token);
+                  // Save the token to the local storage
+                  localStorage.setItem('token', data.token);
+                  router.push('/dashboard');
+                }
+                else {
+                  alert('You don\'t have an examiner account, please contact your admin')
+                }
+        
+              } else {
+                // Handle login failure
+                console.error('Login failed');
+                alert('Incorrect Login details.')
+              }
+        } catch (error) {
+            // Handle network or request error
+            alert('Registration Error, please try again')
+            console.error('Network error:', error);
+        }
+    };
+
+    const handleStudentLogin = () => {
+        // Redirect to the Examiner login page
+        router.push('/'); // Replace '/examiner-login' with the desired examiner login page URL
+    };
+
+
+    return (
+        <div className={styles.body}>
+            <div className={styles.container}>
+
+                <div className={styles.form}>
+                    <Image
+                        src="/assets/logo.svg"
+                        alt="My Logo"
+                        width={400}  // Set your desired width
+                        height={100} // Set your desired height
+                    />
+                    <div className={styles.inputCont}>
+                        <label>Username:</label><br />
+                        <input
+                            type="text"
+                            placeholder="Enter your username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            className={styles.input}
+                        /><br /></div>
+                    <div className={styles.inputCont}>
+
+                        <label>Password:</label><br />
+                        <input
+                            type="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            className={styles.input}
+                            onChange={(e) => setPassword(e.target.value)}
+                        /><br /></div>
+
+                    <div className={styles.inputCont}>
+
+                        <button className={styles.button} style={{ backgroundColor: '#FFA500' }} onClick={handleLogin}>LOGIN</button><br />
+                        <button className={styles.button} style={{ backgroundColor: '#000080' }} onClick={handleStudentLogin}>LOGIN TO STUDENT PORTAL</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default LoginPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,100 @@
 import { useState, useEffect } from 'react';
-import styles from "./page.module.css"
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import styles from "./page.module.css";
+import { API_BASE_URL } from '../constants';
 
 function LoginPage() {
+  const router = useRouter();
+
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleLogin = async () => {
+    // Prepare the login data
+    const loginData = {
+      username: username,
+      password: password,
+    };
+
+    // Send a POST request to your Django API endpoint
+    try {
+      const response = await fetch(`${API_BASE_URL}/login/`, {
+        method: 'POST', headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.user_type === 'student') {
+          // Display the token in the console
+          console.log('Token:', data.token);
+          // Save the token to the local storage
+          localStorage.setItem('token', data.token);
+          router.push('/exam');
+        }
+        else {
+          alert('You don\'t have a student account, please contact your examiner')
+        }
+
+      } else {
+        // Handle login failure
+        console.error('Login failed');
+        alert('Incorrect Login details.')
+      }
+    } catch (error) {
+      // Handle network or request error
+      console.error('Network error:', error);
+    }
+  };
+
+  const handleExaminerLogin = () => {
+    // Redirect to the Examiner login page
+    router.push('/examiner-login'); // Replace '/examiner-login' with the desired examiner login page URL
+  };
+
+
   return (
     <div className={styles.body}>
       <div className={styles.container}>
-        
+
         <div className={styles.form}>
-          <h1 className={styles.title} style={{}}>Login</h1>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className={styles.input}
-          /><br />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            className={styles.input}
-            onChange={(e) => setPassword(e.target.value)}
-          /><br />
-          {/* <button className={styles.button} onClick={handleLogin}>Login</button> */}
+          <div className={styles.imageContainer}>
+            <Image
+              src="/assets/logo.svg"
+              alt="My Logo"
+              width={400}
+              height={100}
+            />
+          </div>
+          <div className={styles.inputCont}>
+            <label>Reg No/Username/Matric No:</label><br />
+            <input
+              type="text"
+              placeholder="e.g. 125/19/2/1193"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className={styles.input}
+            /><br /></div>
+          <div className={styles.inputCont}>
+
+            <label>Password:</label><br />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              className={styles.input}
+              onChange={(e) => setPassword(e.target.value)}
+            /><br /></div>
+
+          <div className={styles.inputCont}>
+
+            <button className={styles.button} style={{ backgroundColor: '#FFA500' }} onClick={handleLogin}>LOGIN</button><br />
+            <button className={styles.button} style={{ backgroundColor: '#000080' }} onClick={handleExaminerLogin}>LOGIN TO EXAMINER PORTAL</button>
+          </div>
         </div>
       </div>
     </div>
