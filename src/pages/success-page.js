8ca469b4b6829b1baaf479e7ed9f5993e1913129
Wizard-Fingerz <adewale.oladiti28@gmail.com>
2024-02-18 09:23:49 -0800

    updated

--- src/pages/success-page.js
+++ src/pages/success-page.js
@@ -1,8 +1,32 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import styles from "./page.module.css";
+
 function SuccessPage() {
-    return(
-        <div>Exam submitted successfully,you can move out of the hall</div>
+    return (
+
+        <div className={styles.body}>
+            <div className={styles.container}>
+
+                <div className={styles.form}>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src="/assets/logo.svg"
+                            alt="My Logo"
+                            width={400}
+                            height={100}
+                        />
+                    </div>
+                    <div className={styles.homeText}>
+                        <h1>Exam submitted successfully, You can move out of the hall</h1>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-    
+
 }
 
 export default SuccessPage;
